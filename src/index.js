290e9ab3ec5cc2666aadd7e078867cc771297ef2
marlonchiu <Knight1898@outlook.com>
2020-02-05 21:19:51 +0800

    支持Sass/Less/Stylus

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './common/style/frame.css'
+import './common/stylus/frame.styl'
 
 ReactDOM.render( < App /> , document.getElementById('root'))
