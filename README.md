32994c82dba6f61c19c57291538ef7d95366c2f3
marlonchiu <Knight1898@outlook.com>
2020-02-05 16:50:59 +0800

    脚手架初始化项目和精简项目

---
+++ README.md
@@ -0,0 +1,4 @@
+# 项目介绍
+
+* [超全面详细一条龙教程！从零搭建React项目全家桶（上篇）](https://juejin.im/post/5e36b72ee51d45307546b29c)
+* [超全面详细一条龙教程！从零搭建React项目全家桶（下篇）](https://juejin.im/post/5e379370f265da3e36457001)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment} from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>This is React App.</h1>
+      </div>
+      <div className="App-other">
+        <h1>This is React App-other.</h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
